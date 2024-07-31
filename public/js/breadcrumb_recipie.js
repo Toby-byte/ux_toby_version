af7fb4059adea3aab7a7fdffb2495df5713757e4
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-07-31 11:32:40 +0200

    added fix for breadcrumbs on frontpage and recipiepage, so timeout function is now removed

--- public/js/breadcrumb_recipie.js
+++ public/js/breadcrumb_recipie.js
@@ -1,20 +1,18 @@
-setTimeout(function () {
-  const currentUrl = window.location.href;
-  const recipieNameElement = document.getElementById("mealName");
-  const element = document.getElementById("current_recipe");
+const currentUrl = window.location.href;
+const recipieNameElement = new URLSearchParams(window.location.search);
+const element = document.getElementById("current_recipe");
 
-  if (recipieNameElement && element) {
-    const recipieName = recipieNameElement.textContent;
-    element.href = currentUrl;
-    element.textContent = recipieName;
-    element.title = recipieName;
-    console.log(recipieName); // This should log the text content of the h1 element
-  } else {
-    if (!recipieNameElement) {
-      console.error("Element with ID 'mealName' not found");
-    }
-    if (!element) {
-      console.error("Element with ID 'current_recipe' not found");
-    }
+if (recipieNameElement && element) { 
+  const recipieName = params.get('name').replace(/"/g,'');
+  element.href = currentUrl;
+  element.textContent = recipieName;
+  element.title = recipieName;
+  console.log(recipieName); // This should log the text content of the h1 element
+} else {
+  if (!recipieNameElement) {
+    console.error("Element with ID 'mealName' not found");
   }
-}, 500); // Delay time in milliseconds
+  if (!element) {
+    console.error("Element with ID 'current_recipe' not found");
+  }
+}
